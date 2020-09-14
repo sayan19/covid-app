b296e2cf8a4bc4a4737c702cafcce7c36ea7656d
sayan19 <soumya@bu.edu>
2020-09-14 13:07:23 -0400

    Added signup with firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 class App extends React.Component {
   constructor() {
     super();
@@ -20,10 +21,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -34,7 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+         <Header currentUser={this.state.currentUser} />
         <Switch>
         <Route exact path='/' component={HomePage} />
        
@@ -46,5 +60,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
